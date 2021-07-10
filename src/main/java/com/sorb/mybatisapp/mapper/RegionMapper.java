8e965c165073c0b49c082fa482ce22ba1771f8fc
Murk <Murk@1.com>
2021-07-11 02:21:13 +0300

    mapper updated for CRUD operations

--- src/main/java/com/sorb/mybatisapp/mapper/RegionMapper.java
+++ src/main/java/com/sorb/mybatisapp/mapper/RegionMapper.java
@@ -1,8 +1,10 @@
 package com.sorb.mybatisapp.mapper;
 
 import com.sorb.mybatisapp.model.Region;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,17 @@ public interface RegionMapper {
 
     @Select("SELECT * FROM region")
     List<Region> getRegions();
+
+    @Select("SELECT * FROM region WHERE id = #{id}")
+    Region getRegionById(int id);
+
+    @Select("INSERT into region (fullName,shortName) VALUES(#{fullName}, #{shortName})")
+    Region addRegion(int id);
+
+    @Update("UPDATE region SET fullName=#{fullName}, shortName =#{shortName} WHERE id =#{id}")
+    void updateRegion(Region region);
+
+    @Delete("DELETE FROM region WHERE id =#{id}")
+    void deleteRegion(int id);
 
 }
