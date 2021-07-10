159c38b312d7b72d6a07a9a527e0548b0f0d1bc2
Murk <Murk@1.com>
2021-07-10 22:21:41 +0300

    initial commit

---
+++ src/main/java/com/sorb/mybatisapp/MybatisAppApplication.java
@@ -0,0 +1,13 @@
+package com.sorb.mybatisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisAppApplication.class, args);
+    }
+
+}
