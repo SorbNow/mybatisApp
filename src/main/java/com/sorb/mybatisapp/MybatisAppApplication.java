e2c0dc31a03677140f2db50c5dd22fddd5359aef
Murk <Murk@1.com>
2021-07-11 17:38:19 +0300

    add cache

--- src/main/java/com/sorb/mybatisapp/MybatisAppApplication.java
+++ src/main/java/com/sorb/mybatisapp/MybatisAppApplication.java
@@ -2,8 +2,10 @@ package com.sorb.mybatisapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MybatisAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sorb/mybatisapp/service/RegionServiceImpl.java
+++ src/main/java/com/sorb/mybatisapp/service/RegionServiceImpl.java
@@ -3,6 +3,10 @@ package com.sorb.mybatisapp.service;
 import com.sorb.mybatisapp.mapper.RegionMapper;
 import com.sorb.mybatisapp.model.Region;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +22,7 @@ public class RegionServiceImpl implements RegionService {
     }
 
     @Override
+    @Cacheable("regions")
     public List<Region> getRegions() {
 
         List<Region> region = regionMapper.getRegions();
@@ -26,6 +31,7 @@ public class RegionServiceImpl implements RegionService {
     }
 
     @Override
+    @Cacheable(value = "regions")
     public Region getRegionById(int id) {
         Region region = regionMapper.getRegionById(id);
         if (region != null) log.info("Region fetched successfully");
@@ -33,6 +39,7 @@ public class RegionServiceImpl implements RegionService {
     }
 
     @Override
+    @CachePut(value = "regions")
     public Integer createRegion(Region region) {
 
         Integer result = regionMapper.addRegion(region);
@@ -41,14 +48,15 @@ public class RegionServiceImpl implements RegionService {
     }
 
     @Override
+    @CachePut(value = "regions", key = "#id")
     public void updateRegion(Region region, int id) {
         log.info("Region with id: " + id + " updated");
         regionMapper.updateRegion(region.getFullName(), region.getShortName(), id);
     }
 
     @Override
+    @CacheEvict(value = "regions")
     public void deleteRegion(int id) {
-
         regionMapper.deleteRegion(id);
         if (regionMapper.getRegionById(id) == null) log.info("Region with id: " + id + " deleted");
     }
